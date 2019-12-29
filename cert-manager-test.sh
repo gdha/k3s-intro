1825b35e78d5724e9eecf803353768fc567da3b6
Gratien D'haese <gratien.dhaese@gmail.com>
2019-12-29 11:36:40 +0100

    adding mysql.yaml and some tweeks to cert-manager-test.sh

--- cert-manager-test.sh
+++ cert-manager-test.sh
@@ -18,6 +18,10 @@ sleep 5
 # See issue at https://github.com/helm/helm/issues/6374
 helm init --service-account tiller --override spec.selector.matchLabels.'name'='tiller',spec.selector.matchLabels.'app'='helm' --output yaml | sed 's@apiVersion: extensions/v1beta1@apiVersion: apps/v1@' | kubectl apply -f -
 
+sleep 5
+helm init
+helm repo update
+
 # helm init --service-account tiller --output yaml | sed 's@apiVersion: extensions/v1beta1@apiVersion: apps/v1@' | sed 's@  replicas: 1@  replicas: 1\n  selector: {"matchLabels": {"app": "helm", "name": "tiller"}}@' | kubectl apply -f -
 
 # Install the CustomResourceDefinition resources separately
