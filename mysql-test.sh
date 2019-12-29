bfbd2b924c213775e47238e5a21e1738d0f19c0c
Gratien D'haese <gratien.dhaese@gmail.com>
2019-12-29 20:06:44 +0100

    updates to mysql-test.sh script

--- mysql-test.sh
+++ mysql-test.sh
@@ -14,11 +14,28 @@ kubectl get pods -n mysql
 echo "Port forwarding localhost:3306 to connect to mysql database"
 kubectl port-forward svc/local-database-mysql 3306 -n mysql &
 
+echo "Expose port 3306 to external IP"
+kubectl expose deployment --name=mysql-svc local-database-mysql --type=LoadBalancer -n mysql
+
+echo "Show the services of mysql"
+kubectl get services -n mysql
+
+echo "On which worker node is the database running?"
+MYSQL_POD="$(kubectl get pods -n mysql | grep ^local | awk '{print $1}')"
+kubectl get pods -n mysql $MYSQL_POD -o wide
+
 echo "Define some required variables before we can connect to database"
 export MYSQL_HOST=127.0.0.1
 export MYSQL_PORT=3306
 export MYSQL_ROOT_PASSWORD=r00tpassw0rd
 
-echo "Connecting to database"
+echo "Connecting to database on $(hostname)"
+echo "Type \"exit;\" to exit from database"
 mysql -h $MYSQL_HOST -P$MYSQL_PORT -u root -p$MYSQL_ROOT_PASSWORD
 
+echo "-----------------------------------------------------------------------------"
+echo "Connect to mysql database via host system (mysql command must be installed) :"
+echo "Copy/paste the following on another windows"
+echo "MYSQL_HOST=$(kubectl describe pods -n mysql $MYSQL_POD | grep -i node: | cut -d/ -f2)"
+echo "MYSQL_ROOT_PASSWORD=r00tpassw0rd"
+echo "mysql -h $MYSQL_HOST -P$MYSQL_PORT -u root -p$MYSQL_ROOT_PASSWORD"
